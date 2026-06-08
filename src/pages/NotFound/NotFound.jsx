aeb1a9c73e9a6f40446dcc959dedf649ab350ecc
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-08 19:45:52 -0400

    feat[errorPage]: added 404 notification to page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router';
 function NotFound() {
   return (
     <>
+      <h1>404</h1>
+      <h2>Page does not exist for this site</h2>
       <p>Page not found</p>
       <Link to="/">Go back home</Link>
     </>
