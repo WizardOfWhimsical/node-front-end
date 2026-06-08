1a8f8e9407c2261f17995ed4fd3223a0770c5283
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-08 19:48:27 -0400

    feat[stats]: BarChart made

---
+++ src/pages/Profile/BarChart.jsx
@@ -0,0 +1,68 @@
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend
+);
+
+//https://www.geeksforgeeks.org/reactjs/how-to-implement-barchart-in-reactjs/
+export default function BarChart({ total, active, completed, name }) {
+  const data = {
+    labels: ['Total', 'Active', 'Completed'],
+    datasets: [
+      {
+        label: 'To-Dos',
+        data: [total, active, completed],
+        backgroundColor: ['#50b432', '#058DC7', '#ED561B'],
+        borderColor: ['#50b432', '#058DC7', '#ED561B'],
+        borderWidth: 1,
+        borderRadius: 5,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 3,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        title: { display: true, text: ['here i am', 'another one'] },
+        labels: {
+          font: { size: 18 },
+        },
+      },
+      title: {
+        display: true,
+        text: `${name}'s To-Do Stats`,
+        font: { size: 20 },
+      },
+    },
+  };
+
+  return (
+    <div style={{ maxWidth: '650px', height: '400px' }}>
+      <Bar data={data} options={options} />
+    </div>
+  );
+}
