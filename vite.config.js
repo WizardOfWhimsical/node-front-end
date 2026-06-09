47e37e0dda5a08729fa6e4c51a318256291477f7
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-09 16:26:31 -0400

    chore:disabled eslint in config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default ({ mode }) => {
+  // eslint-disable-next-line
   const env = loadEnv(mode, process.cwd(), '');
   return defineConfig({
     plugins: [react()],
