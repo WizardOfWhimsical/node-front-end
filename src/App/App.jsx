c5f84084649309e6e3d62f510e277ef9eba7110b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-12 12:48:34 -0400

    feat: moved app to utilize module for css and index for exporting

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,19 +2,19 @@ import { useReducer } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 import './App.css';
-import TodosPage from './pages/TodosPage/TodosPage';
-import About from './pages/About/About';
-import Header from './shared/Header';
-import NotFound from './pages/NotFound/NotFound';
-import Logon from './pages/Logon/Logon';
-import Register from './pages/Register/Register';
-import AuthPlaceholder from './features/AuthPlaceholder/AuthPlaceholder';
-import StatsPage from './pages/Profile/ProfilePage';
+import TodosPage from '../pages/TodosPage/TodosPage';
+import About from '../pages/About/About';
+import Header from '../shared/Header';
+import NotFound from '../pages/NotFound/NotFound';
+import Logon from '../pages/Logon/Logon';
+import Register from '../pages/Register/Register';
+import AuthPlaceholder from '../features/AuthPlaceholder/AuthPlaceholder';
+import StatsPage from '../pages/Profile/ProfilePage';
 import {
   reducer as userReducer,
   initialState as initialUserState,
   context as UserContext,
-} from './reducers/user.reducer';
+} from '../reducers/user.reducer';
 
 // const token = `Bearer ${import.meta.env.VITE_PAT}`;
 
