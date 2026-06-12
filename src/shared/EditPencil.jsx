27bff428f978aaed0fa9887d0e521cc89f47309f
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-11 22:24:59 -0400

    feat: expoted

--- src/shared/EditPencil.jsx
+++ src/shared/EditPencil.jsx
@@ -1,12 +1,12 @@
 import { LuPencil, LuEraser } from 'react-icons';
-function ToolBar({ pencilOnClick, eraserOnClick }) {
+export default function ToolBar({ pencilOnClick, eraserOnClick }) {
   return (
     <div className="toolbarContainer">
       <button onClick={pencilOnClick}>
         <LuPencil size={24} color="#333333" />
       </button>
       <button onClick={eraserOnClick}>
-        <LuEraser size={24} color="#333333" />
+        <LuEraser size={24} color="#222222" />
       </button>
     </div>
   );
