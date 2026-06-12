255d2fe7efea0302c465b65b5c11bf1966f569c8
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-12 12:53:33 -0400

    chore: changed import path to reflect new dir

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
