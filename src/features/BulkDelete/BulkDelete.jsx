2d9449390e6566365b9f873a710c45b9aaf2b7be
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-12 23:42:57 -0400

    feat: front end response for bulk delete

--- src/features/BulkDelete/BulkDelete.jsx
+++ src/features/BulkDelete/BulkDelete.jsx
@@ -1,11 +1,33 @@
+import { useContext } from 'react';
+import { context as UserContext } from '../../reducers/user.reducer.js';
 function BulkDelete(props) {
-  const idsToDelete = props.todoState.todoList
-    .filter((todo) => todo.isCompleted === true)
-    .map((t) => t.id);
+  const { userState } = useContext(UserContext);
+  async function handleOnclick() {
+    const response = window.confirm('Are you sure?');
+
+    if (response) {
+      const idsToDelete = props.todoState.todoList
+        .filter((todo) => todo.isCompleted === true)
+        .map((t) => t.id);
+      try {
+        await fetch('/api/tasks/bulk', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': userState?.userData?.csrfToken,
+          },
+          body: JSON.stringify({ tasks: idsToDelete }),
+          credentials: 'include',
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
   return (
     <div>
-      <button type="button" onClick={() => console.log({ idsToDelete })}>
+      <button type="button" onClick={handleOnclick}>
         [BULKDELETE]
       </button>
     </div>
