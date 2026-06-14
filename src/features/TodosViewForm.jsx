117489e0c0d680efd6caa2d6d9242ff8cd93ad24
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-14 00:07:14 -0400

    done: sunday rest

--- src/features/TodosViewForm.jsx
+++ src/features/TodosViewForm.jsx
@@ -40,29 +40,33 @@ function TodosViewForm({
           Clear
         </button>
       </div>
-      <div>
-        <label htmlFor="sortBy">Sort by: </label>
-        <select
-          name="sortBy"
-          id="sortBy"
-          onChange={(e) => setSortField(e.target.value)}
-          value={sortField}
-        >
-          <option value="title">Title</option>
-          <option value="createdAt">Time Added</option>
-          <option value="isCompleted">Is Completed</option>
-        </select>
-
-        <label htmlFor="direction">Direction: </label>
-        <select
-          name="direction"
-          id="direction"
-          value={sortDirection}
-          onChange={(e) => setSortDirection(e.target.value)}
-        >
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
+      <div className="center">
+        {/* seperate labels from select */}
+        <label htmlFor="sortBy" className="justifyInbetween">
+          Sort by:
+          <select
+            name="sortBy"
+            id="sortBy"
+            onChange={(e) => setSortField(e.target.value)}
+            value={sortField}
+          >
+            <option value="title">Title</option>
+            <option value="createdAt">Time Added</option>
+            <option value="isCompleted">Is Completed</option>
+          </select>
+        </label>
+        <label htmlFor="direction" className="justifyInbetween">
+          Direction:
+          <select
+            name="direction"
+            id="direction"
+            value={sortDirection}
+            onChange={(e) => setSortDirection(e.target.value)}
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
       </div>
     </form>
   );
