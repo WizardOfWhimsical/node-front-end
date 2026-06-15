4cc109e0b3b6877ba9a1e6dfd9ddb45a69d4b6ac
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-15 14:24:11 -0400

    fix: made message more genaric

--- src/shared/MessageDisplay/MessageDisplay.jsx
+++ src/shared/MessageDisplay/MessageDisplay.jsx
@@ -11,7 +11,7 @@ export default function MessageDisplay({
     <div className={styles.errorOverlay}>
       <div className={styles.errorContainer}>
         <p className={styles.errorMessage}>
-          {error ? 'A problem occured:' : 'Delete all Complete?'}
+          {error ? 'A problem occured:' : 'Please Confirm?'}
           <br />
           {error || message}
         </p>
